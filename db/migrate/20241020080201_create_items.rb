d1428513d57cd3ddff66595a6dd2c046d2d1f3d7
mri-na <marina.89260106@gmail.com>
2024-10-20 18:34:16 +0900

    ActiveStorage導入

--- db/migrate/20241020080201_create_items.rb
+++ db/migrate/20241020080201_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-
+      t.references :user, null: false, foreign_key: true
+      t.string :product_name, null: false
+      t.integer :category_id, null: false
+      t.integer :price, null: false
+      t.integer :shipping_fee_id, null: false
+      t.text :explanation, null: false
+      t.integer :condition_id, null: false
+      t.integer :sender_address_id, null: false
+      t.integer :shipping_date_id, null: false
       t.timestamps
     end
   end
