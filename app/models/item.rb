467e8f2ae2b24faaab2f06e6b393944ddcdf113b
mri-na <marina.89260106@gmail.com>
2024-10-21 19:48:24 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :sender_address, :shipping_date, :shipping_fee
+  
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  validates :product_name, :price, :explanation, presence: true
+  validates :category_id, :condition_id, :shipping_fee_id, :sender_address_id, :shipping_date_id, numericality: { other_than: 1 } 
 end
