709870727a93e29e8b8473f6224f2d42b7ed596a
mri-na <marina.89260106@gmail.com>
2024-10-23 00:37:53 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,6 @@ class Item < ApplicationRecord
 
   validates :product_name, :price, :explanation, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :sender_address_id, :shipping_date_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :order_number, format: { with: /\A[0-9]+\z/, message: '半角数字のみ入力できます' }
 end
