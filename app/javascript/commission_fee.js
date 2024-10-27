5d066f673780ad4cf859c1b106a5946a17f46c2c
mri-na <marina.89260106@gmail.com>
2024-10-27 15:01:35 +0900

    イベント発火

--- app/javascript/commission_fee.js
+++ app/javascript/commission_fee.js
@@ -1,3 +1,15 @@
-window.addEventListener('turbo:load', () => {
-  console.log("OK");
-});
+const price = () => {
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const addTaxPrice = document.getElementById("profit");
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+      console.log(inputValue);
+      addTaxDom.innerHTML = Math.floor(inputValue * 0.1 );
+      addTaxPrice.innerHTML = Math.floor( inputValue - inputValue * 0.1 );
+  })
+  };
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+  
--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   validates :category_id, :condition_id, :shipping_fee_id, :sender_address_id, :shipping_date_id, numericality: { other_than: 1 } 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :price, format: { with: /\A[0-9]+\z/, message: '半角数字のみ入力できます' }
-  validates :content, presence: true, unless: :was_attached?
+  validates :image, presence: true, unless: :was_attached?
 
   def was_attached?
     self.image.attached?
