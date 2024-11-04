f5f077a9e576401d62aa09311d5b1d500b8b2c60
mri-na <marina.89260106@gmail.com>
2024-11-04 21:42:40 +0900

    商品情報編集ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit]
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items_exist = Item.exists?
@@ -24,10 +25,30 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:product_name, :category_id, :price, :shipping_fee_id, :explanation, :condition_id,
                                  :sender_address_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    return if @item.user == current_user
+
+    redirect_to root_path
   end
 end
