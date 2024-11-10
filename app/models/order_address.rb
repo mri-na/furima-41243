272ffe1f357ac310e673900bee8e43c7b0fc051b
mri-na <marina.89260106@gmail.com>
2024-11-10 17:07:40 +0900

    Formオブジェクト作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,17 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :order_id, :postal_code, :sender_address_id, :city, :street_number, :building_name,
+                :telephone_number
+
+  with_options presence: true do
+    validates :user_id, :item_id, :order_id, :sender_address_id, :city, :street_number
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :telephone_number, format: { with: /\A\d{10,11}\z/, message: 'は10桁以上、11桁以内の半角数字でハイフンなしで入力してください' }
+  end
+  validates :building_name, allow_blank: true
+end
+
+def save
+  order = Order.create(user_id:, item_id:)
+  Address.create(order_id:, postal_code:, sender_address_id:, city:, street_number:, building_name:, telephone_number:)
+end
